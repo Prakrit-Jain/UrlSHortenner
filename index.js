4757502a8fcd6c0ed35007a9631874887a8d0842
Prakrit-Jain <96251253+Prakrit-Jain@users.noreply.github.com>
2022-04-12 18:08:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const connectToMongo = require("./database/db");
+let ejs = require('ejs');
+const urlData = require("./Models/urldata");
+const app = express();
+const path = require("path")
+const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.set("views", path.join(__dirname, "views"));
+connectToMongo();
+
+let ourSite = "http://localhost:5000/";
+
+function validateURL(link) {
+    if (link.indexOf("http://") == 0 || link.indexOf("https://") == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+app.get("/", (req, res) => {
+    res.render("home", { shortenUrl: null });
+})
+
+//Generating short url
+app.post("/", urlencodedParser, async (req, res) => {
+    let { url } = req.body;
+    if (!validateURL(url)) {
+        url = "http://" + url;
+    }
+    try {
+        const data = await urlData.findOne({ url: url });
+        if (data) {
+            const shortenUrl = ourSite.concat(data.shortcode);
+            res.render("home", { shortenUrl: shortenUrl });
+        } else {
+            let shortcode = Math.random().toString(36).substring(2, 7);
+            const newdata = new urlData({ url, shortcode });
+            const savedData = await newdata.save();
+            const shortenUrl = ourSite.concat(shortcode);
+            res.render("home", { shortenUrl: shortenUrl })
+        }
+    } catch (error) {
+        console.log(error);
+        res.render("error", { errorCode: "500", errorMsg: "Internal Server Error!" })
+    }
+
+})
+
+//Redirecting to the Shorten Url
+app.get("/:scode", async (req, res) => {
+    const shortCode = req.params.scode;
+    try {
+        const data = await urlData.findOne({ shortcode: shortCode });
+        if (data) {
+            res.redirect(data.url);
+        } else {
+            res.render("error", { errorCode: "404", errorMsg: "Not Found!" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.render("error", { errorCode: "500", errorMsg: "Internal Server Error!" })
+    }
+})
+
+
+
+app.listen(5000, (req, res) => {
+    console.log("Server running at 5000");
+})
